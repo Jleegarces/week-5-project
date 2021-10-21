7672112accef7eac8aead145066aea4fe4cdc743
Jonnathan Garces <jonnathangarces@Jonnathans-MacBook-Pro.local>
2021-10-20 23:41:08 -0400

    new branch

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config:
+
+    SECRET_KEY = "Roni"
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
